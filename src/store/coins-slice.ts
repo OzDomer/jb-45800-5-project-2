65f220483a480214ab4bb7e733ff9670da1bbe36
Oz Domer <ozdomer7@gmail.com>
2026-04-21 19:42:35 +0300

    store setup

---
+++ src/store/coins-slice.ts
@@ -0,0 +1,22 @@
+import { createSlice ,type PayloadAction } from "@reduxjs/toolkit"
+import type Coins from "../models/Coins"
+
+interface CoinsState{
+    coins: Coins[]
+}
+
+const initialState: CoinsState = {
+    coins: []
+}
+const coinsSlice = createSlice({
+    name: "coins",
+    initialState,
+    reducers: {
+        populate: (state, action: PayloadAction<Coins[]>) => {
+            state.coins = action.payload
+        }
+    }
+})
+export const { populate } = coinsSlice.actions
+
+export default coinsSlice.reducer
---
+++ src/store/user-coins-slice.ts
@@ -0,0 +1,28 @@
+import { createSlice ,type PayloadAction } from "@reduxjs/toolkit"
+
+interface SelectedCoin {
+    coinId: string,
+    coinSymbol: string
+}
+interface UserCoins {
+    UserCoins: SelectedCoin[]
+}
+
+const initialState: UserCoins = {
+    UserCoins:[]
+}
+const userCoinsSlice = createSlice({
+    name: "userCoins",
+    initialState,
+    reducers: {
+        populate: (state, action: PayloadAction<SelectedCoin>) => {
+            state.UserCoins.push(action.payload)
+        },
+        removeCoin: (state, action: PayloadAction<string>) => {
+            state.UserCoins = state.UserCoins.filter(coin => coin.coinId !== action.payload)
+        }
+    }
+})
+export const { populate, removeCoin } = userCoinsSlice.actions
+
+export default userCoinsSlice.reducer
