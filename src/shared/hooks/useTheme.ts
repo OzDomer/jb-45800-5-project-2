48dad44bf89148c305229f39380754077d3d73dc
Oz Domer <ozdomer7@gmail.com>
2026-04-27 15:56:43 +0300

    added a first render guard to useTheme hook

--- src/shared/hooks/useTheme.ts
+++ src/shared/hooks/useTheme.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { getItemFromStorage, saveToStorage } from "../utils/storage"
 
 type Theme = "light" | "dark"
@@ -8,10 +8,14 @@ const STORAGE_KEY = "cryptonite:theme"
 
 export function useTheme() {
     const [theme, setTheme] = useState<Theme>(() => getItemFromStorage<Theme>(STORAGE_KEY, "dark"))
-
+    // using useref for first run since theme already being initialized on boot  
+    const isFirstRun = useRef(true)
     useEffect(() => {
         document.documentElement.dataset.theme = theme
+        if(!isFirstRun.current){
         saveToStorage(STORAGE_KEY, theme)
+        }
+        isFirstRun.current = false
     },[theme])
 
     function toggleTheme() {
--- src/shared/layout/Header.tsx
+++ src/shared/layout/Header.tsx
@@ -1,17 +1,20 @@
 import { NavLink } from "react-router-dom"
 import "./Header.css"
+import { useTheme } from "../hooks/useTheme"
 
 export default function Header() {
+    const { theme, toggleTheme } = useTheme()
 
-    return(
+    return (
         <div className="Header">
-        HEADER RARHAHRHAR
-        <div className="NavLinks">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/reports">Reports</NavLink>
-            <NavLink to="/ai">Ai Recommendations</NavLink>
-            <NavLink to="/about">About</NavLink>
+            HEADER RARHAHRHAR
+            <div className="NavLinks">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/reports">Reports</NavLink>
+                <NavLink to="/ai">Ai Recommendations</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <button onClick={toggleTheme}>Toggle {theme}</button>
+            </div>
         </div>
-        </div>
-        )
+    )
 }
