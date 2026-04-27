80935ee3c25238150349b0cfff683fd843018cc0
Oz Domer <ozdomer7@gmail.com>
2026-04-27 10:25:08 +0300

    wired toggle function to add and remove coins from watchlist

--- src/features/home/CoinCard.tsx
+++ src/features/home/CoinCard.tsx
@@ -1,5 +1,5 @@
 import { useAppDispatch, useAppSelector } from "../../shared/store/hooks"
-import { addCoin } from "../../shared/store/user-coins-slice"
+import { addCoin, removeCoin } from "../../shared/store/user-coins-slice"
 import "./CoinCard.css"
 import type Coin from "../../shared/models/Coin"
 import MoreInfo from "./MoreInfo"
@@ -13,9 +13,17 @@ interface CoinCardProps {
 export default function CoinCard({ coinCard, isOpen, onMoreInfoClick }: CoinCardProps) {
     const { id, symbol, name, image, price_change_percentage_24h } = coinCard
     const userCoins = useAppSelector(state => state.userCoinsSlice.userCoins)
+    const isInWatchlist = userCoins.some(coin => coin.coinId === id)
     const dispatch = useAppDispatch()
     function addCoinToWatchlist() {
-        dispatch(addCoin({ coinId: id, coinSymbol: symbol.toUpperCase() , name, image}))
+        dispatch(addCoin({ coinId: id, coinSymbol: symbol.toUpperCase(), name, image }))
+    }
+    function removeCoinFromWatchlist(coinId: string) {
+        dispatch(removeCoin(coinId))
+    }
+    function handleWatchlistToggle() {
+        if (isInWatchlist) removeCoinFromWatchlist(id)
+        else addCoinToWatchlist()
     }
 
     return (
@@ -26,8 +34,8 @@ export default function CoinCard({ coinCard, isOpen, onMoreInfoClick }: CoinCard
                 NAME: {name}
                 <img src={image} style={{ width: 100, height: 100 }} />
                 PRICE CHANGE RARARA: {price_change_percentage_24h}
-                <button onClick={addCoinToWatchlist}>Add coin to watchlist</button>
-                 <button onClick={onMoreInfoClick}> {isOpen ? "close more information" : "open more information"}</button>
+                <button onClick={handleWatchlistToggle}>{isInWatchlist ? "Remove coin from watchlist" : "Add coin to watch list"}</button>
+                <button onClick={onMoreInfoClick}> {isOpen ? "close more information" : "open more information"}</button>
                 {isOpen &&
                     <MoreInfo coinId={id} />}
             </div>
